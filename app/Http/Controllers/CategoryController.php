b310058547f9f07548ae04f5c7c809e3f237d25c
asghar.ali <asghar.ali@zonalgo.com>
2022-02-02 15:56:09 +0500

    make controller,model,migration

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all(['id','title','description']);
+        return response()->json($categories);
+    }
+
+    
+    public function store(Request $request)
+    {
+        $category = Category::create($request->post());
+        return response()->json([
+            'message'=>'Category Created Successfully!!',
+            'category'=>$category
+        ]);
+    }
+
+
+    public function show(Category $category)
+    {
+        return response()->json($category);
+    }
+
+
+    public function update(Request $request, Category $category)
+    {
+        $category->fill($request->post())->save();
+        return response()->json([
+            'message'=>'Category Updated Successfully!!',
+            'category'=>$category
+        ]);
+    }
+
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->json([
+            'message'=>'Category Deleted Successfully!!'
+        ]);
+    }
+}
